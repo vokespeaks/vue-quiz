66f4e43f8a531a13e1f92974f545874aa19b29b4
voke junior <vefe44@gmail.com>
2019-12-21 01:42:54 +0100

    complete the quiz app

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled>Fancy Quiz</b-nav-item>
+      <b-nav-item disabled>Count {{numCorrect}} / {{numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"]
+};
+</script>
